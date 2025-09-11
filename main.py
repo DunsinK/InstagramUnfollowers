3dcd4b389b1527a232355bd6a179935a5f783f3e
DunsinK <133840905+DunsinK@users.noreply.github.com>
2025-09-11 13:38:06 -0500

    pyscript module update
    Testing and fixed module export error. When ran on a server the index.html can use pyscript to run and execute python

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 from bs4 import BeautifulSoup
-not_following_back = {}
-with open('following.html','r') as following:
-    followString = following.read()
-    
-    with open('followers_1.html','r') as followers:
-        followersString = followers.read()
+from js import document
 
-        followersSoup = BeautifulSoup(followersString, 'lxml') # makes soup object
-        followers_name_tags = followersSoup.find_all('a') # finds all name <a> tags
-        followingSoup = BeautifulSoup(followString, 'lxml') 
-        following_name_tags = followingSoup.find_all('a') 
-        for tag in following_name_tags:
-            #print(tag.text)
-            not_following_back[tag.text] = 0
-        for tag in followers_name_tags:
-            try:
-                del not_following_back[tag.text]  
-            except:
-                continue 
-        for key in not_following_back:
-            print(key)
+def notFollowingBack():
+    not_following_back = {}
+    print("main.py ran")
+    with open('following.html','r') as following:
+        followString = following.read()
         
+        with open('followers_1.html','r') as followers:
+            followersString = followers.read()
+
+            followersSoup = BeautifulSoup(followersString, 'lxml') # makes soup object
+            followers_name_tags = followersSoup.find_all('a') # finds all name <a> tags
+            followingSoup = BeautifulSoup(followString, 'lxml') 
+            following_name_tags = followingSoup.find_all('a') 
+            for tag in following_name_tags:
+                #print(tag.text)
+                not_following_back[tag.text] = 0
+            for tag in followers_name_tags:
+                try:
+                    del not_following_back[tag.text]  
+                except:
+                    continue 
+            for key in not_following_back:
+                print(key)
+print("im running!")
+document.getElementByID("process-btn").onclick = notFollowingBack
 
 #How the Program works
 # Import your follower and following html files then adjust the html in the open method to be accurate  
